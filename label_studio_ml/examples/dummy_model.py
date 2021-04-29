cb8d0624e2a13f46f4c44bdc540a8e0277f2f349
Max Tkachenko <makseq@gmail.com>
2021-04-30 01:07:53 +0300

    Update dummy_model with comments

--- label_studio_ml/examples/dummy_model.py
+++ label_studio_ml/examples/dummy_model.py
@@ -7,13 +7,19 @@ class DummyModel(LabelStudioMLBase):
 
     def __init__(self, **kwargs):
         super(DummyModel, self).__init__(**kwargs)
-
+        
+        # pre-initialize your variables here
         from_name, schema = list(self.parsed_label_config.items())[0]
         self.from_name = from_name
         self.to_name = schema['to_name'][0]
         self.labels = schema['labels']
 
     def predict(self, tasks, **kwargs):
+        """ This is where inference happens: 
+            model returns the list of predictions based on input list of tasks
+            
+            :param tasks: Label Studio tasks in JSON format
+        """
         results = []
         for task in tasks:
             results.append({
@@ -30,4 +36,11 @@ class DummyModel(LabelStudioMLBase):
         return results
 
     def fit(self, completions, workdir=None, **kwargs):
+        """ This is where training happens: train your model given list of completions,
+            then returns dict with created links and resources
+
+            :param completions: aka annotations, the labeling results from Label Studio 
+            :param workdir: current working directory for ML backend
+        """
+        # save some training outputs to the job result
         return {'random': random.randint(1, 10)}
