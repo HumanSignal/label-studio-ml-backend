232a375f5fb4e31aca6be6da84c254cbde44798b
KonstantinKorotaev <konstantin@heartex.com>
2021-09-16 14:46:29 +0300

    {fix] Fix context and add score for result

--- label_studio_ml/examples/substring_matching/substring_matching.py
+++ label_studio_ml/examples/substring_matching/substring_matching.py
@@ -4,6 +4,8 @@ import re
 from label_studio_ml.model import LabelStudioMLBase
 import random
 import string
+import functools
+import requests
 
 logger = logging.getLogger(__name__)
 
@@ -15,7 +17,9 @@ class SubstringMatcher(LabelStudioMLBase):
     def predict(self, tasks, **kwargs):
         # extract task meta data: labels, from_name, to_name and other
         task = tasks[0]
-        meta = self._extract_meta(task)
+        context = kwargs.get('context')
+        result = context.get('result')[0]
+        meta = self._extract_meta({**task, **result})
         # if no meta data extracted return empty list
         if not meta:
             return []
@@ -25,6 +29,7 @@ class SubstringMatcher(LabelStudioMLBase):
             return []
         # construct results from extracted data
         results = []
+        avg_score = 0
         for item in extracted_data:
             if item['start'] == meta['start'] and item['end'] == meta['end']:
                 continue
@@ -41,25 +46,33 @@ class SubstringMatcher(LabelStudioMLBase):
                     'start': item['start'],
                     'end': item['end'],
                     meta['type']: meta['labels'],
-                }
+                },
+                'score': item['score']
             }
             results.append(temp)
+            avg_score += item['score']
 
         return [{
-            'result': results
+            'result': results,
+            'score': avg_score / max(len(extracted_data)-1, 1)
         }]
 
     @staticmethod
     def _extract_data(data, value):
         result = []
+        if data.startswith('http://') or data.startswith('https://'):
+            data = requests.get(data).text()
         low = value.lower()
         low_data = data.lower()
         for m in re.finditer(low, low_data):
             start = m.start()
+            d = data[start:start + len(low)]
+            score = functools.reduce(lambda a, b: a+b, [1 if k[0] == k[1] else 0 for k in zip(value, d)]) / len(d)
             temp = {
                 'start': start,
                 'end': start + len(low),
-                'text': data[start:start + len(low)]
+                'text': d,
+                'score': score
             }
             result.append(temp)
         return result
@@ -67,16 +80,14 @@ class SubstringMatcher(LabelStudioMLBase):
     @staticmethod
     def _extract_meta(task):
         meta = dict()
-        annotation = task.get('annotations')
-        if annotation:
-            data = annotation[0]['result'][0]
-            meta['id'] = data['id']
-            meta['from_name'] = data['from_name']
-            meta['to_name'] = data['to_name']
-            meta['type'] = data['type']
-            meta['labels'] = data['value'][data['type']]
-            meta['value'] = data['value']['text']
+        if task:
+            meta['id'] = task['id']
+            meta['from_name'] = task['from_name']
+            meta['to_name'] = task['to_name']
+            meta['type'] = task['type']
+            meta['labels'] = task['value'][task['type']]
+            meta['value'] = task['value']['text']
             meta['data'] = list(task['data'].values())[0]
-            meta['start'] = data['value']['start']
-            meta['end'] = data['value']['end']
+            meta['start'] = task['value']['start']
+            meta['end'] = task['value']['end']
         return meta
