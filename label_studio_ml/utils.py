1def30a5f236c493d8beb60229d33a7895931ed9
Konstantin Korotaev <42615530+KonstantinKorotaev@users.noreply.github.com>
2021-12-14 19:58:23 +0300

    Switch to label-studio-tools

--- label_studio_ml/utils.py
+++ label_studio_ml/utils.py
@@ -1,17 +1,9 @@
-import json
-import os
-import sys
-import urllib
-import hashlib
-import requests
 import logging
-import io
 
-from appdirs import user_cache_dir, user_data_dir
-from urllib.parse import urlparse
 from PIL import Image
 
 from label_studio_tools.core.utils.params import get_env
+from label_studio_tools.core.utils.io import get_local_path
 
 DATA_UNDEFINED_NAME = '$undefined$'
 
@@ -20,22 +12,6 @@ logger = logging.getLogger(__name__)
 _LABEL_TAGS = {'Label', 'Choice'}
 _NOT_CONTROL_TAGS = {'Filter',}
 _DIR_APP_NAME = 'label-studio'
-
-
-if sys.platform.startswith('java'):
-    import platform
-    os_name = platform.java_ver()[3][0]
-    if os_name.startswith('Windows'): # "Windows XP", "Windows 7", etc.
-        system = 'win32'
-    elif os_name.startswith('Mac'): # "Mac OS X", etc.
-        system = 'darwin'
-    else: # "Linux", "SunOS", "FreeBSD", etc.
-        # Setting this to "linux2" is not ideal, but only Windows or Mac
-        # are actually checked for and the rest of the module expects
-        # *sys.platform* style strings.
-        system = 'linux2'
-else:
-    system = sys.platform
 
 
 def get_single_tag_keys(parsed_label_config, control_type, object_type):
@@ -74,104 +50,9 @@ def get_image_local_path(url, image_cache_dir=None, project_dir=None, image_dir=
     return get_local_path(url, image_cache_dir, project_dir, get_env('HOSTNAME'), image_dir)
 
 
-def get_local_path(url, cache_dir=None, project_dir=None, hostname=None, image_dir=None, access_token=None):
-    is_local_file = url.startswith('/data/') and '?d=' in url
-    is_uploaded_file = url.startswith('/data/upload')
-    if image_dir is None:
-        upload_dir = os.path.join(get_data_dir(), 'media', 'upload')
-        image_dir = project_dir and os.path.join(project_dir, 'upload') or upload_dir
-
-    # File reference created with --allow-serving-local-files option
-    if is_local_file:
-        filename, dir_path = url.split('/data/')[1].split('?d=')
-        dir_path = str(urllib.parse.unquote(dir_path))
-        filepath = os.path.join(dir_path, filename)
-        if not os.path.exists(filepath):
-            raise FileNotFoundError(filepath)
-        return filepath
-
-    # File uploaded via import UI
-    elif is_uploaded_file and os.path.exists(image_dir):
-        filepath = url.replace("/data/upload", image_dir)
-        return filepath
-
-    elif is_uploaded_file and hostname:
-        url = hostname + url
-        logger.info('Resolving url using hostname [' + hostname + '] from LSB: ' + url)
-
-    elif is_uploaded_file:
-        raise FileNotFoundError("Can't resolve url, neither hostname or project_dir passed: " + url)
-
-    if is_uploaded_file and not access_token:
-        raise FileNotFoundError("Can't access file, no access_token provided for Label Studio Backend")
-
-    # File specified by remote URL - download and cache it
-    cache_dir = cache_dir or get_cache_dir()
-    parsed_url = urlparse(url)
-    url_filename = os.path.basename(parsed_url.path)
-    url_hash = hashlib.md5(url.encode()).hexdigest()[:6]
-    filepath = os.path.join(cache_dir, url_hash + '__' + url_filename)
-    if not os.path.exists(filepath):
-        logger.info('Download {url} to {filepath}'.format(url=url, filepath=filepath))
-        headers = {'Authorization': 'Token ' + access_token} if access_token else {}
-        r = requests.get(url, stream=True, headers=headers)
-        r.raise_for_status()
-        with io.open(filepath, mode='wb') as fout:
-            fout.write(r.content)
-    return filepath
-
-
 def get_image_size(filepath):
     return Image.open(filepath).size
 
 
 def get_bool_env(key, default):
     return get_env(key, default, is_bool=True)
-
-
-def bool_from_request(params, key, default):
-    """ Get boolean value from request GET, POST, etc
-
-    :param params: dict POST, GET, etc
-    :param key: key to find
-    :param default: default value
-    :return: boolean
-    """
-    value = params.get(key, default)
-
-    if isinstance(value, str):
-        value = cast_bool_from_str(value)
-
-    return bool(int(value))
-
-
-def cast_bool_from_str(value):
-    if value.lower() in ['true', 'yes', 'on', '1']:
-        value = True
-    elif value.lower() in ['false', 'no', 'not', 'off', '0']:
-        value = False
-    else:
-        raise ValueError(f'Incorrect value in = "{value}". '
-                         f'It should be one of [1, 0, true, false, yes, no]')
-    return value
-
-
-def get_data_dir():
-    data_dir = user_data_dir(appname=_DIR_APP_NAME)
-    os.makedirs(data_dir, exist_ok=True)
-    return data_dir
-
-
-def get_cache_dir():
-    cache_dir = user_cache_dir(appname=_DIR_APP_NAME)
-    os.makedirs(cache_dir, exist_ok=True)
-    return cache_dir
-
-
-def json_load(file, int_keys=False):
-    with io.open(file, encoding='utf8') as f:
-        data = json.load(f)
-        if int_keys:
-            return {int(k): v for k, v in data.items()}
-        else:
-            return data
