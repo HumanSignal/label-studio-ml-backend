6d67ee6d4004c78a496e82d140c75a92f464f8b0
Konstantin Korotaev <42615530+KonstantinKorotaev@users.noreply.github.com>
2022-06-10 13:50:50 +0300

    Update test_basic.py

--- label_studio_ml/tests/test_basic.py
+++ label_studio_ml/tests/test_basic.py
@@ -2,4 +2,8 @@ import requests
 
 
 def test_basic_health_check():
-    pass
+    response = requests.get("http://127.0.0.1:9090")
+    assert response.status_code == 200
+
+    response = requests.get("http://127.0.0.1:9090/health")
+    assert response.status_code == 200
