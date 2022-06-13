448c0fa25edafb224b28750df02d36195f52d813
Konstantin Korotaev <42615530+KonstantinKorotaev@users.noreply.github.com>
2022-06-13 16:33:17 +0300

    Change test.yml to use matrix.ml-backend

--- label_studio_ml/tests/test_basic.py
+++ label_studio_ml/tests/test_basic.py
@@ -5,6 +5,7 @@ import os
 ML_BACKEND = os.environ.get("ML_BACKEND", "")
 
 
+@pytest.mark.first
 def test_basic_health_check():
     response = requests.get("http://127.0.0.1:9090/")
     assert response.status_code == 200
@@ -13,8 +14,9 @@ def test_basic_health_check():
     assert response.status_code == 200
 
 
+@pytest.mark.second
 @pytest.mark.skipif(ML_BACKEND != "simple_text_classifier", reason="Test for simple_text_classifier")
-def test_setup():
+def test_setup_simple_text_classifier():
     data = {
         "project": "1.1654592272",
         'schema': '<View><Text name="text" value="$transcript"/><View style="box-shadow: 2px 2px 5px #999;                padding: 20px; margin-top: 2em;                border-radius: 5px;"><Header value="Choose text sentiment"/><Choices name="sentiment" toName="text" choice="single" showInLine="true"><Choice value="Positive"/><Choice value="Negative"/><Choice value="Neutral"/></Choices></View></View>',
@@ -25,13 +27,18 @@ def test_setup():
     assert response.status_code == 200
 
 
-def test_predict():
+@pytest.mark.skipif(ML_BACKEND != "simple_text_classifier", reason="Test for simple_text_classifier")
+def test_predict_simple_text_classifier():
+    data = {}
+    response = requests.post("http://127.0.0.1:9090/setup", json=data)
+    assert response.status_code == 200
+
+
+@pytest.mark.skipif(ML_BACKEND != "simple_text_classifier", reason="Test for simple_text_classifier")
+def test_webhook_predict_simple_text_classifier():
     pass
 
 
-def test_webhook_predict():
-    pass
-
-
-def test_train():
+@pytest.mark.skipif(ML_BACKEND != "simple_text_classifier", reason="Test for simple_text_classifier")
+def test_train_simple_text_classifier():
     pass
