334957269adfd7b9c394e43bd69c1b0f787f736b
Shivansh <106922533+shondle@users.noreply.github.com>
2023-04-25 15:03:02 +0000

    Add ONNX usability and eraser feature

--- label_studio_ml/examples/segment_anything_model/labelstudio.py
+++ label_studio_ml/examples/segment_anything_model/labelstudio.py
@@ -1,31 +1,46 @@
+import torch
+from torchvision import transforms
+import matplotlib.pyplot as plt
+from segment_anything import sam_model_registry, SamPredictor
+from segment_anything.utils.onnx import SamOnnxModel
 from label_studio_ml.model import LabelStudioMLBase
 from label_studio_converter import brush
-from segment_anything import SamPredictor, sam_model_registry, SamAutomaticMaskGenerator
 from label_studio_ml.utils import get_image_local_path
 import numpy as np
 import cv2
-import matplotlib.pyplot as plt
-import random
-import string
-import datetime
 import os
+from PIL import Image
+import string
+import random
+
+import onnxruntime
+from onnxruntime.quantization import QuantType
+from onnxruntime.quantization.quantize import quantize_dynamic
 
 def load_my_model():
         """
         Loads the Segment Anything model on initializing Label studio, so if you call it outside MyModel it doesn't load every time you try to make a prediction
         Returns the predictor object. For more, look at Facebook's SAM docs
         """
-        # if you're not using CUDA, use "cpu" instead
-        device = "cuda"
-
-        # Note: YOU MUST HAVE THE MODEL SAVED IN THE SAME DIRECTORY AS YOUR BACKEND
-        sam = sam_model_registry["vit_h"](checkpoint="sam_vit_h_4b8939.pth")
-
+        device = "cuda"     # if you're not using CUDA, use "cpu" instead .... good luck not burning your computer lol
+        sam = sam_model_registry["vit_h"](checkpoint="sam_vit_h_4b8939.pth")        # Note: YOU MUST HAVE THE MODEL SAVED IN THE SAME DIRECTORY AS YOUR BACKEND
         sam.to(device=device)
+
         predictor = SamPredictor(sam)
         return predictor
 
 PREDICTOR = load_my_model()
+PREV_IMG_PATH = ""
+PREV_IMG = 0
+IMAGE_EMBEDDING = 0
+previous_id = 0
+
+# empty mask and indicator for no mask
+onnx_mask_input = np.zeros((1, 1, 256, 256), dtype=np.float32)
+onnx_has_mask_input = np.zeros(1, dtype=np.float32)
+
+onnx_model_path = "sam_onnx_quantized_example.onnx"
+ORT = onnxruntime.InferenceSession(onnx_model_path)
 
 class MyModel(LabelStudioMLBase):
     def __init__(self, **kwargs):
@@ -36,6 +51,9 @@ class MyModel(LabelStudioMLBase):
     
     def predict(self, tasks, **kwargs):
         """ Returns the predicted mask for a smart keypoint that has been placed."""
+        
+        # Use this to check times for your predictions
+        # print(f"Current data and time1: {str(datetime.datetime.now())}") # Current data and time1: 2023-04-16 18:56:09.361688 (ALMOST INSTANTANEOUS FROM THE RUN)
 
         results = []
         predictions = []
@@ -43,6 +61,7 @@ class MyModel(LabelStudioMLBase):
 
         image_url = tasks[0]['data']['image']
 
+        print(f"Here are the kwargs now {kwargs}")
 
         # getting the height and width of the image that you are annotating real-time 
         height = kwargs['context']['result'][0]['original_height']
@@ -54,46 +73,128 @@ class MyModel(LabelStudioMLBase):
 
         # label that you selected with the keypoint. If this is running into error, use the second line of code instead
         label = kwargs['context']['result'][0]['value']['labels'][0]
-        # keypointlabel = kwargs['context']['result'][0]['value']['keypointlabels'][0]
+        keypointlabel = kwargs['context']['result'][0]['value']['keypointlabels'][0]
 
 
         # loading the image you are annotating in local. The image_dir MUST MATCH THE DIRECTORY WHERE YOU IMPORTED YOUR ANNOTATIONS INTO LABEL STUDIO FROM
         image_path = get_image_local_path(image_url, image_dir=os.getcwd())
-        split = image_path.split('-')[-1]
-        image = cv2.imread(f"./{split}")
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-        # retriving predictions from SAM. For more info, look at Facebook's SAM docs
-        predictor.set_image(image)
 
-        masks, scores, logits = predictor.predict(
-            point_coords=np.array([[int(x), int(y)]]),
-            point_labels=np.array([1]),
-            multimask_output=False,
-        )
+        # this is to speed up inference after the first time you selected an image
+        global PREV_IMG_PATH
+        global PREV_IMG
+        global IMAGE_EMBEDDING
+        global onnx_has_mask_input
+        global onnx_mask_input
+        global previous_id
 
-        mask = masks[0].astype(np.uint8) # each mask has shape [H, W]
+        if image_path != PREV_IMG_PATH:
+            PREV_IMG_PATH = image_path
+            split = image_path.split('-')[-1]
+            image = cv2.imread(f"./{split}")
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            PREV_IMG = image
+            # retrieving predictions from SAM. For more info, look at Facebook's SAM docs
+            predictor.set_image(image)
 
-        # converting the mask from the model to RLE format which is usable in Label Studio
-        mask = mask * 255
-        rle = brush.mask2rle(mask)
+            image_embedding = predictor.get_image_embedding()
+            IMAGE_EMBEDDING = image_embedding.cpu().numpy()
+        elif image_path == PREV_IMG_PATH:
+            image = PREV_IMG
+            image_embedding = IMAGE_EMBEDDING
+
+        input_point = np.array([[int(x), int(y)]])
+        input_label = np.array([1])
+
+        onnx_coord = np.concatenate([input_point, np.array([[0.0, 0.0]])], axis=0)[None, :, :]
+        onnx_label = np.concatenate([input_label, np.array([-1])], axis=0)[None, :].astype(np.float32)
+
+        onnx_coord = predictor.transform.apply_coords(onnx_coord, image.shape[:2]).astype(np.float32)
+
+        # Package to run in onnx
+        ort_inputs = {
+            "image_embeddings": IMAGE_EMBEDDING,
+            "point_coords": onnx_coord,
+            "point_labels": onnx_label,
+            "mask_input": onnx_mask_input,
+            "has_mask_input": onnx_has_mask_input,
+            "orig_im_size": np.array(image.shape[:2], dtype=np.float32)
+        }
+
+        # Predict and threshold mask
+        masks, _, low_res_logits = ORT.run(None, ort_inputs)
+        masks = masks > predictor.model.mask_threshold
+
+        mask = masks[0, 0, :, :].astype(np.uint8) # each mask has shape [H, W]
+
+        adjusted_masks = []
+
+        # check if SAM eraser is being used
+        if 'Eraser' in keypointlabel:
+            print(f"the keypoint label is {keypointlabel}")
+            print("we have made it into the if statement")
+            prev_rle = []
+            previous_id = []
+            type = " ".join(keypointlabel.split()[:-1])
+            indexes = np.nonzero(mask)
+            print(f"indexes are {indexes}") # lots of stuff here to 1000 for x and y
+
+            for i in tasks[0]['drafts']:
+                print()
+                if tasks[0]['drafts'][0]['result'][0]['value']['brushlabels'][0] == type:
+                    print("we have made it into the 1111 if statement")
+                    prev_rle.append(tasks[0]['drafts'][0]['result'][0]['value']['rle'])
+            print("we have made it into the 1111 FOR statement")
+            prev_mask = brush.decode_rle(prev_rle[-1])
+            print(f"the shape of the prev mask is {prev_mask.shape}")
+            prev_mask = np.reshape(prev_mask, [height, width, 4])[:, :, 3]
+            prev_mask = (prev_mask/255).astype(np.uint8)
+
+            indices = np.logical_and(mask == 1, prev_mask == 1)
+            prev_mask[indices] = 0
+                        
+            rle_resubmit = prev_mask * 255
+            rle_resubmit = brush.mask2rle(rle_resubmit)
+            adjusted_masks.append(rle_resubmit)
+            
+            results.append({
+                "from_name": self.from_name,
+                "to_name": self.to_name,
+                "original_width": width,
+                "original_height": height,
+                "image_rotation": 0,
+                "value": {
+                    "format": "rle",
+                    "rle": adjusted_masks[0],
+                    "brushlabels": [type],
+                },
+                "type": "brushlabels",
+                "id": ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.ascii_lowercase + string.digits)), # creates a random ID for your label everytime so no chance for errors,
+                "readonly": False,
+            })
+        else:
+            label_id = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.ascii_lowercase + string.digits)), # creates a random ID for your label everytime so no chance for errors
+            
+            # converting the mask from the model to RLE format which is usable in Label Studio
+            mask = mask * 255
+            rle = brush.mask2rle(mask)
+            results.append({
+                "from_name": self.from_name,
+                "to_name": self.to_name,
+                "original_width": width,
+                "original_height": height,
+                "image_rotation": 0,
+                "value": {
+                    "format": "rle",
+                    "rle": rle,
+                    "brushlabels": [label],
+                },
+                "type": "brushlabels",
+                "id": label_id,
+                "readonly": False,
+            })
 
         # returning the result from the prediction and passing it to show on the front-end
-        results.append({
-            "from_name": self.from_name,
-            "to_name": self.to_name,
-            "original_width": width,
-            "original_height": height,
-            "image_rotation": 0,
-            "value": {
-                "format": "rle",
-                "rle": rle,
-                "brushlabels": [label],
-            },
-            "type": "brushlabels",
-            "id": ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.ascii_lowercase + string.digits)), # creates a random ID for your label every time
-            "readonly": False,
-        })
         predictions.append({"result": results,
                             "model_version": "vit_h"
         })
