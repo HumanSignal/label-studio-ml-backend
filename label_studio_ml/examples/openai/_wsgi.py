5e0db76c72de0fcb8b6d904d0946f1c389f42c5a
nik <nik@heartex.net>
2023-07-11 20:31:03 +0100

    Make OpenAIPredictor more generic

--- label_studio_ml/examples/openai/_wsgi.py
+++ label_studio_ml/examples/openai/_wsgi.py
@@ -29,7 +29,7 @@ logging.config.dictConfig({
 })
 
 from label_studio_ml.api import init_app
-from openai_predictor import OpenAIPrecitor
+from openai_predictor import OpenAIPredictor
 
 
 _DEFAULT_CONFIG_PATH = os.path.join(os.path.dirname(__file__), 'config.json')
@@ -102,11 +102,11 @@ if __name__ == "__main__":
         kwargs.update(parse_kwargs())
 
     if args.check:
-        print('Check "' + OpenAIPrecitor.__name__ + '" instance creation..')
-        model = OpenAIPrecitor(**kwargs)
+        print('Check "' + OpenAIPredictor.__name__ + '" instance creation..')
+        model = OpenAIPredictor(**kwargs)
 
     app = init_app(
-        model_class=OpenAIPrecitor,
+        model_class=OpenAIPredictor,
         model_dir=os.environ.get('MODEL_DIR', args.model_dir),
         redis_queue=os.environ.get('RQ_QUEUE_NAME', 'default'),
         redis_host=os.environ.get('REDIS_HOST', 'localhost'),
@@ -119,7 +119,7 @@ if __name__ == "__main__":
 else:
     # for uWSGI use
     app = init_app(
-        model_class=OpenAIPrecitor,
+        model_class=OpenAIPredictor,
         model_dir=os.environ.get('MODEL_DIR', os.path.dirname(__file__)),
         redis_queue=os.environ.get('RQ_QUEUE_NAME', 'default'),
         redis_host=os.environ.get('REDIS_HOST', 'localhost'),
