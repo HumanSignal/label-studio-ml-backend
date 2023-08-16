cc3614a84f889ddbdef8f9d0c354d025c96f763c
Sergey Zhuk <sergey.zhuk@humansignal.com>
2023-08-16 18:30:50 +0100

    SAM model: convert onnx during buildtime

--- label_studio_ml/examples/segment_anything_model/start.sh
+++ label_studio_ml/examples/segment_anything_model/start.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-# Run converter for ONNX
-if [ "$SAM_CHOICE" == "ONNX" ]; then
-  python3 onnxconverter.py
-fi
-
 # Execute the gunicorn command
 exec gunicorn --preload --bind :$PORT --workers 1 --threads 8 --timeout 0 _wsgi:app
