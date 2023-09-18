f14005f08d48d477dde4af76b970043e8f838fb9
shondle <imshivansh@tutamail.com>
2023-09-18 12:51:34 -0500

    Add boilerplate

---
+++ label_studio_ml/examples/yolov8/model.py
@@ -0,0 +1,62 @@
+from typing import List, Dict, Optional
+from label_studio_ml.model import LabelStudioMLBase
+
+from ultralytics import YOLO
+
+
+class NewModel(LabelStudioMLBase):
+
+    def predict(self, tasks: List[Dict], context: Optional[Dict] = None, **kwargs) -> List[Dict]:
+        """ Write your inference logic here
+            :param tasks: [Label Studio tasks in JSON format](https://labelstud.io/guide/task_format.html)
+            :param context: [Label Studio context in JSON format](https://labelstud.io/guide/ml.html#Passing-data-to-ML-backend)
+            :return predictions: [Predictions array in JSON format](https://labelstud.io/guide/export.html#Raw-JSON-format-of-completed-tasks)
+        """
+        print(f'''\
+        Run prediction on {tasks}
+        Received context: {context}
+        Project ID: {self.project_id}
+        Label config: {self.label_config}
+        Parsed JSON Label config: {self.parsed_label_config}''')
+
+        # TODO: here figure out what type of prediction we are looking for -> classification, segmentation, bounding boxes, etc.
+        context = "classification"
+
+        if context=="classification":
+            model = YOLO('yolov8n-cls.pt')
+
+
+        img = 'https://ultralytics.com/images/bus.jpg'
+
+        
+        results = model(img)
+
+
+
+
+        return []
+
+    def fit(self, event, data, **kwargs):
+        """
+        This method is called each time an annotation is created or updated
+        You can run your logic here to update the model and persist it to the cache
+        It is not recommended to perform long-running operations here, as it will block the main thread
+        Instead, consider running a separate process or a thread (like RQ worker) to perform the training
+        :param event: event type can be ('ANNOTATION_CREATED', 'ANNOTATION_UPDATED')
+        :param data: the payload received from the event (check [Webhook event reference](https://labelstud.io/guide/webhook_reference.html))
+        """
+
+        # use cache to retrieve the data from the previous fit() runs
+        old_data = self.get('my_data')
+        old_model_version = self.get('model_version')
+        print(f'Old data: {old_data}')
+        print(f'Old model version: {old_model_version}')
+
+        # store new data to the cache
+        self.set('my_data', 'my_new_data_value')
+        self.set('model_version', 'my_new_model_version')
+        print(f'New data: {self.get("my_data")}')
+        print(f'New model version: {self.get("model_version")}')
+
+        print('fit() completed successfully.')
+
