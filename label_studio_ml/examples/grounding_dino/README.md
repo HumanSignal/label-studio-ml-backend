bd5bb918e9f37d82e8bcf77a0b453438dc2e2d21
Shivansh <imshivansh@tutamail.com>
2023-09-28 23:48:34 -0500

    Feature/shondle sam with dino (#352)
    * Add baseline SAM code
    
    * Add TO DOs
    
    * Add SAM integration fixes
    
    * Add regular SAM
    
    * Add SAM capability and instructions
    
    * Update README.md

--- label_studio_ml/examples/grounding_dino/README.md
+++ label_studio_ml/examples/grounding_dino/README.md
@@ -1,9 +1,10 @@
-https://github.com/HumanSignal/label-studio-ml-backend/assets/106922533/470c6cfc-bb2e-4fe3-9627-30a7e3bc28d5
+https://github.com/HumanSignal/label-studio-ml-backend/assets/106922533/d1d2f233-d7c0-40ac-ba6f-368c3c01fd36
+
 
 
 ## GroundingDINO Backend Integration
 
-Use text prompts for zero-shot detection of objects in images! Specify the detection of any object and get State of the Art results without any model fine tuning!
+Use text prompts for zero-shot detection of objects in images! Specify the detection of any object and get State of the Art results without any model fine tuning! In addition, get segmentation predictions from SAM with just text prompts!
 
 See [here](https://github.com/IDEA-Research/GroundingDINO) for more details about the pretrained GroundingDINO model. 
 
@@ -11,7 +12,7 @@ See [here](https://github.com/IDEA-Research/GroundingDINO) for more details abou
 Quickstart
 
 1. Make sure docker is installed
-2. Edit `docker-compose.yml` to include your LABEL_STUDIO_ACCESS_TOKEN found in teh Label Studio software, and the LABEL_STUDIO_HOST which includes the address on which the frontend is hosted on.
+2. Edit `docker-compose.yml` to include your LABEL_STUDIO_ACCESS_TOKEN found in the Label Studio software, and the LABEL_STUDIO_HOST which includes the address on which the frontend is hosted on.
 
 Example-
 - `LABEL_STUDIO_HOST=http://123.456.7.8:8080`
@@ -40,3 +41,12 @@ Example-
 This may be adjusted to your needs, but please keep the promp section and some rectangle labels.
 
 6. Go to an image task in one of your projects. Turn on the Auto-annotation switch. Then, type in the prompt box and press add. After this, you should receive your predictions. See the video above for a demo. 
+
+
+## Using GroundingSAM
+
+Combine the Segment Anything Model with your text input to automatically generate mask predictions! 
+
+To do this, set `USE_SAM=True` before running. 
+
+If you want to use a more efficient version of SAM, set `USE_MOBILE_SAM=True` as well.
--- label_studio_ml/examples/grounding_dino/dino.py
+++ label_studio_ml/examples/grounding_dino/dino.py
@@ -19,10 +19,31 @@ import numpy as np
 # LOADING THE MODEL
 groundingdino_model = load_model("./GroundingDINO/groundingdino/config/GroundingDINO_SwinT_OGC.py", "./GroundingDINO/weights/groundingdino_swint_ogc.pth")
 
+
 BOX_TRESHOLD = os.environ.get("BOX_THRESHOLD", 0.3)
 TEXT_TRESHOLD = os.environ.get("TEXT_THRESHOLD", 0.25)
 LABEL_STUDIO_ACCESS_TOKEN = os.environ.get("LABEL_STUDIO_ACCESS_TOKEN")
 LABEL_STUDIO_HOST = os.environ.get("LABEL_STUDIO_HOST")
+
+USE_SAM = os.environ.get("USE_SAM", False)
+USE_MOBILE_SAM = os.environ.get("USE_MOBILE_SAM", False)
+
+MOBILESAM_CHECKPOINT = os.environ.get("MOBILESAM_CHECKPOINT", "mobile_sam.pt")
+SAM_CHECKPOINT = os.environ.get("SAM_CHECKPOINT", "sam_vit_h_4b8939.pth")
+
+
+
+if USE_MOBILE_SAM:
+    from mobile_sam import SamPredictor, sam_model_registry
+
+    model_checkpoint = MOBILESAM_CHECKPOINT
+    reg_key = 'vit_t'
+elif USE_SAM:
+    from segment_anything import SamPredictor, sam_model_registry
+
+    model_checkpoint = SAM_CHECKPOINT
+    reg_key = 'vit_h'
+
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -36,16 +57,21 @@ class DINOBackend(LabelStudioMLBase):
 
         self.from_name, self.to_name, self.value = None, None, None
 
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+
+        if USE_MOBILE_SAM or USE_SAM:
+            sam = sam_model_registry[reg_key](checkpoint=model_checkpoint)
+            sam.to(device=self.device)
+            self.predictor = SamPredictor(sam)    
+            
 
     def predict(self, tasks: List[Dict], context: Optional[Dict] = None, **kwargs) -> List[Dict]:
 
-        
         self.from_name, self.to_name, self.value = self.get_first_tag_occurence('RectangleLabels', 'Image')
-
 
         TEXT_PROMPT = context['result'][0]['value']['text'][0]
 
-        self.label = TEXT_PROMPT
+        self.label = TEXT_PROMPT.strip("_SAM") # make sure that using as text prompt allows you to label it a certain way
 
         
         all_points = []
@@ -87,12 +113,14 @@ class DINOBackend(LabelStudioMLBase):
                 all_scores.append(logit)
                 all_lengths.append((H, W))
 
-
-        predictions = self.get_results(all_points, all_scores, all_lengths, self.from_name, self.to_name, self.label)
+        if USE_MOBILE_SAM or USE_SAM:
+            predictions = self.get_sam_results(img_path, all_points, all_lengths)
+        else:
+            predictions = self.get_results(all_points, all_scores, all_lengths)
         
         return predictions
 
-    def get_results(self, all_points, all_scores, all_lengths, from_name, to_name, label):
+    def get_results(self, all_points, all_scores, all_lengths):
         
         results = []
         
@@ -104,14 +132,14 @@ class DINOBackend(LabelStudioMLBase):
             
             results.append({
                 'id': label_id,
-                'from_name': from_name,
-                'to_name': to_name,
+                'from_name': self.from_name,
+                'to_name': self.to_name,
                 'original_width': width,
                 'original_height': height,
                 'image_rotation': 0,
                 'value': {
                     'rotation': 0,
-                    'rectanglelabels': [label],
+                    'rectanglelabels': [self.label],
                     'width': (points[2] - points[0]) / width * 100,
                     'height': (points[3] - points[1]) / height * 100,
                     'x': points[0] / width * 100,
@@ -126,25 +154,59 @@ class DINOBackend(LabelStudioMLBase):
         return [{
             'result': results
         }]
-    
-if __name__ == '__main__':
-    # test the model
-    model = DINOBackend()
-    model.use_label_config('''
-    <View>
-        <Image name="image" value="$image" zoom="true"/>
-        <RectangleLabels name="tag" toName="image">
-            <Label value="Fire" background="#FF0000"/>
-        </RectangleLabels>
-    </View>
-    ''')
-    results = model.predict(
-        tasks=[{
-            'data': {
-                # 'image': 'https://s3.amazonaws.com/htx-pub/datasets/images/125245483_152578129892066_7843809718842085333_n.jpg'
-                'image': 'label_studio_ml/examples/grounding_dino/fire.jpg'
-            }}],
-        context= None
-    )
-    # results[0]['result'][0]['value']['rle'] = f'...{len(results[0]["result"][0]["value"]["rle"])} integers...'
-    # print(json.dumps(results, indent=2))
+
+    def get_sam_results(
+        self,
+        img_path,
+        input_boxes,
+        lengths
+    ):
+        image = cv2.imread(img_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        self.predictor.set_image(image)
+
+        input_boxes = torch.from_numpy(np.array(input_boxes))
+
+        
+        transformed_boxes = transformed_boxes = self.predictor.transform.apply_boxes_torch(input_boxes, image.shape[:2])
+        masks, probs, _ = self.predictor.predict_torch(
+            point_coords=None,
+            point_labels=None,
+            boxes=transformed_boxes,
+            multimask_output=False,
+        )
+
+        masks = masks[:, 0, :, :].cpu().numpy().astype(np.uint8)
+        probs = probs.cpu().numpy()
+        
+        results = []
+
+
+        for mask, prob, length in zip(masks, probs, lengths):
+            height, width = length
+            # creates a random ID for your label everytime so no chance for errors
+            label_id = str(uuid4())[:4]
+
+            # converting the mask from the model to RLE format which is usable in Label Studio
+            mask = mask * 255
+            rle = brush.mask2rle(mask)
+
+            results.append({
+                'id': label_id,
+                'from_name': self.from_name,
+                'to_name': self.to_name,
+                'original_width': width,
+                'original_height': height,
+                'image_rotation': 0,
+                'value': {
+                    'format': 'rle',
+                    'rle': rle,
+                    'brushlabels': [self.label],
+                },
+                'score': float(prob[0]),
+                'type': 'brushlabels',
+                'readonly': False
+            })
+        return [{
+            'result': results
+        }]
