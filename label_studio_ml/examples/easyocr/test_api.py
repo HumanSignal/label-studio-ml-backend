71a50c07886d295e60f5065f44136505fb05185e
nik <nik@heartex.net>
2024-05-06 15:43:18 +0100

    easyocr test: Fix image content, add temp dir fixture, remove prints

--- label_studio_ml/examples/easyocr/test_api.py
+++ label_studio_ml/examples/easyocr/test_api.py
@@ -26,18 +26,26 @@ def client():
         yield client
 
 
+@pytest.fixture
+def model_dir_env(tmp_path, monkeypatch):
+    model_dir = tmp_path / "model_dir"
+    model_dir.mkdir()
+    monkeypatch.setattr(EasyOCR, 'MODEL_DIR', str(model_dir))
+    return model_dir
+
+
 @responses.activate
-def test_predict(client):
+def test_predict(client, model_dir_env):
     responses.add(
         responses.GET,
-        'http://test_predict.easyocr.ml-backend.com/image.png',
+        'http://test_predict.easyocr.ml-backend.com/image.jpeg',
         body=open(os.path.join(os.path.dirname(__file__), 'test_images', 'image.jpeg'), 'rb').read(),
         status=200
     )
     request = {
         'tasks': [{
             'data': {
-                'image': 'http://test_predict.easyocr.ml-backend.com/image.png'
+                'image': 'http://test_predict.easyocr.ml-backend.com/image.jpeg'
             }
         }],
         # Your labeling configuration here
@@ -69,7 +77,6 @@ def test_predict(client):
     response = client.post('/predict', data=json.dumps(request), content_type='application/json')
     assert response.status_code == 200
     response = json.loads(response.data)
-    print('!!!!', response)
     expected_texts = {
         'IZIN SOLUTIONS',
         'HlRlS',
