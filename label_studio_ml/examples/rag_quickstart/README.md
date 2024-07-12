eee64bcb342cb0f1ce9a7b2008186beee470b253
Jo Booth <jo.m.booth@gmail.com>
2024-07-12 00:46:08 -0400

    format, refactor, add RAGAS

--- label_studio_ml/examples/rag_quickstart/README.md
+++ label_studio_ml/examples/rag_quickstart/README.md
@@ -1,13 +1,13 @@
 <!--
 ---
-title: Interactive LLM labeling with GPT
+title: Question answering with RAG using Label Studio
 type: guide
 tier: all
 order: 5
 hide_menu: true
 hide_frontmatter_title: true
-meta_title: Interactive LLM labeling with OpenAI, Azure, or Ollama
-meta_description: Label Studio tutorial for interactive LLM labeling with OpenAI, Azure, or Ollama
+meta_title: RAG labeling with OpenAI using Label Studio
+meta_description: Label Studio tutorial for RAG labeling with OpenAI and LangChain
 categories:
     - Generative AI
     - Large Language Model
@@ -15,61 +15,55 @@ categories:
     - Azure
     - Ollama
     - ChatGPT
+    - RAG
+    - LangChain
+    - Embeddings
 image: "/tutorials/llm-interactive.png"
 ---
 -->
 
-# Interactive LLM labeling
+# RAG Quickstart Labeling
 
-This example server connects Label Studio to [OpenAI](https://platform.openai.com/), [Ollama](https://ollama.com/),
-or [Azure](https://azure.microsoft.com/en-us/products/ai-services/openai-service) API to interact with GPT chat models (
-gpt-3.5-turbo, gpt-4, etc.).
+This example server connects Label Studio to [OpenAI](https://platform.openai.com/), to interact with chat and embedding models. It supports question answering and evaluation using RAG, given a list of questions as tasks, and a folder containing documentation (eg, a /docs path within a Github repository.)
 
-The interactive flow allows you to perform the following scenarios:
+## Starting the ML Backend
 
-* Autolabel data given an LLM prompt (e.g. "Classify this text as sarcastic or not")
-* Collect pairs of user prompts and response inputs to fine tune your own LLM.
-* Automate data collection and summarization over image documents.
-* Create a RLHF (Reinforcement Learning from Human Feedback) loop to improve the LLM's performance.
-* Evaluate the LLM's performance.
+1. Make your reference documentation available to the backend
 
-Check the [Generative AI templates](https://labelstud.io/templates/gallery_generative_ai) section for more examples.
+Create a `docker-compose.override.yml` file alongside `docker-compose.yml`, and use it to mount a folder containing your documentation into the filesystem of the ML Backend's image. This example will mount the folder at /host/path/to/your/documentation on your host, to the path /data/documentation inside the ML Backend docker image. The DOCUMENTATION_PATH and DOCUMENTATION_GLOB settings given below will match all `.md` files within /data/documentation (or its subfolders).
 
-## Quickstart
+```
+  rag_quickstart:
+    volumes:
+      - /host/path/to/your/documentation:/data/documentation
+    environment:
+      - DOCUMENTATION_PATH=/data/documentation
+      - DOCUMENTATION_GLOB=**/*.md
+      - LABEL_STUDIO_API_KEY=<your label studio API key>
+      - OPENAI_API_KEY=<your OpenAI API key>
+```
 
-1. Build and start the Machine Learning backend on `http://localhost:9090` <br /><br />
+2. Build and start the Machine Learning backend on `http://localhost:9090` <br /><br />
 ```bash
 docker-compose up
 ```
 
-2. Check if it works: <br /><br />
+3. Check if it works: <br /><br />
  ```bash
 $ curl http://localhost:9090/health
 {"status":"UP"}
 ```
 
-3. Open a Label Studio project and go to **Settings > Model**. [Connect the model](https://labelstud.io/guide/ml#Connect-the-model-to-Label-Studio), specifying `http://localhost:9090` as the URL.
+4. Open a Label Studio project and go to **Settings > Model**. [Connect the model](https://labelstud.io/guide/ml#Connect-the-model-to-Label-Studio), specifying `http://localhost:9090` as the URL.
 
    Ensure the **Interactive preannotations** toggle is enabled and click **Validate and Save**.
-4. The project config should be compatible with the ML backend. This ML backend can support various input data formats
-   like plain text, hypertext, images, and structured dialogs. To ensure the project config is compatible, follow these
-   rules:
-
-   - The project should contain at least one `<TextArea>` tag to be used as a prompt input. To specify which `<TextArea>` tag  to use, set the `PROMPT_PREFIX` environment variable.
-   For example, if your labeling config includes `<TextArea name="prompt" ...>`, then you would specify `PROMPT_PREFIX=prompt`.
-   - The project should contain at least one input data tag from the following list of supported tags: `<Text>`, `<Image>`, `<HyperText>`, `<Paragraphs>`.
-   - If you want to capture the generated LLM response as a label, your labeling config should contain a `<Choices>` tag.
-   For example, `<Choices name="choices" ...>`.
-   - If you want to set the default prompt to be shown before the user input, you can set the `DEFAULT_PROMPT` environment variable. For example, `DEFAULT_PROMPT="Classify this text as sarcastic or not. Text: {text}, Labels: {labels}"` or `DEFAULT_PROMPT=/path/to/prompt.txt`.
-
-    Note that the default prompt isn't supported with `USE_INTERNAL_PROMPT_TEMPLATE=1` mode, so you will need to set `USE_INTERNAL_PROMPT_TEMPLATE=0` to use default prompt. You can use the fields from `task['data']` in the prompt template, as well as special `{labels}` field to show the list of available labels.
-
-5. Open a task and ensure the **Auto-Annotation** toggle is enabled (it is located at the bottom of the labeling interface).
-6. Enter a prompt in the prompt input field and press `Shift+Enter`. The LLM response will be generated and displayed in
+5. Use the label config below. The config and backend can be customized to fit your needs.
+6. Open a task and ensure the **Auto-Annotation** toggle is enabled (it is located at the bottom of the labeling interface).
+7. Enter a prompt in the prompt input field and press `Shift+Enter`. The LLM response will be generated and displayed in
    the response field.
-7. If you want to apply LLM auto-annotation to multiple tasks at once, go to the [Data Manager](https://labelstud.io/guide/manage_data), select a group of tasks and then select **Actions > Retrieve Predictions** (or **Batch Predictions** in Label Studio Enterprise).
+8. If you want to apply LLM auto-annotation to multiple tasks at once, go to the [Data Manager](https://labelstud.io/guide/manage_data), select a group of tasks and then select **Actions > Retrieve Predictions** (or **Batch Predictions** in Label Studio Enterprise).
 
-## Configuration examples
+## Configuration
 
 <View>
     <Style>
@@ -83,7 +77,7 @@ $ curl http://localhost:9090/health
         line-height: 1.6;
         font-size: 16px;
         }
-        .evaluation input {
+        .ragas input {
             background: none;
             border: none;
             padding: 0;
@@ -91,7 +85,7 @@ $ curl http://localhost:9090/health
             font-size: 20px;
             font-weight: 600;
         }
-        .evaluation input::-webkit-inner-spin-button {
+        .ragas input::-webkit-inner-spin-button {
           -webkit-appearance: none;
           margin: 0;
         }
@@ -120,24 +114,37 @@ $ curl http://localhost:9090/health
               smart="false"
               placeholder="Generated response will appear here..."
     />
-    <View className="evaluation" >
+  	<View className="ragas" >
     <View style="display: flex;">
-      <Header style="padding-right: 1em;" value="Evaluation (scored 0 to 100):"/><Number name="float_eval" toName="context" defaultValue="0" />
+      <Header style="padding-right: 1em;" value="RAGAS evaluation (averaged, 0 to 100):"/><Number name="float_eval" toName="context" defaultValue="0" />
     </View>
-    <TextArea name="evaluation"
+    <TextArea name="ragas"
               toName="context"
-              rows="4"
+              rows="2"
               maxSubmissions="1"
               showSubmitButton="false"
               smart="false"
-              placeholder="Generated evaluation will appear here..."
+              placeholder="RAGAS evaluation will appear here..."
+    />
+  	</View>
+    <View className="evaluation" >
+    <View style="display: flex;">
+      <Header style="padding-right: 1em;" value="Textual evaluation:"/>
+    </View>
+    <TextArea name="evaluation"
+              toName="context"
+              rows="2"
+              maxSubmissions="1"
+              showSubmitButton="false"
+              smart="false"
+              placeholder="Textual evaluation will appear here..."
     />
     </View>
     <Header value="Documentation:"/>
     <View className="documentation">
     <TextArea name="documentation"
               toName="context"
-              rows="0"
+              rows="2"
               maxSubmissions="1"
               showSubmitButton="false"
               smart="false"
@@ -155,61 +162,3 @@ $ curl http://localhost:9090/health
   }
 }
 ```
-
-## Parameters
-
-When deploying the server, you can specify the following parameters as environment variables:
-
-- `DEFAULT_PROMPT`: Define a default prompt to be shown before the user input. For example, `DEFAULT_PROMPT="Classify this text as sarcastic or not. Text: {text}, Labels: {labels}"` or `DEFAULT_PROMPT=/path/to/prompt.txt`.
-
-    Note that `USE_INTERNAL_PROMPT_TEMPLATE` should be set to `0` if you are setting a default prompt.
-
-- `PROMPT_PREFIX` (default: `prompt`): An identifier for the prompt input field. For example, if you set
-  `PROMPT_PREFIX` to `my-prompt`, the following input field will be used for the
-  prompt: `<TextArea name="my-prompt" ...>`.
-
-- `USE_INTERNAL_PROMPT_TEMPLATE` (default: `1`). If set to `1`, the server will use the internal prompt template. If set
-  to
-  `0`, the server will use the prompt template provided in the input prompt.
-
-- `PROMPT_TEMPLATE` (default: `"Source Text: {text}\n\nTask Directive: {prompt}"`): The prompt template to use:
-
-  - If `USE_INTERNAL_PROMPT_TEMPLATE` is set to `1`, the server will use
-  the default internal prompt template.
-
-  - If `USE_INTERNAL_PROMPT_TEMPLATE` is set to `0`, the server will use the prompt template provided
-  in the input prompt (i.e. the user input from `<TextArea name="my-prompt" ...>`).
-
-  In the later case, the user has to provide the placeholders that match input task fields. For example, if the user wants to use the `input_text` and `instruction` field from the input task `{"input_text": "user text", "instruction": "user instruction"}`, the user has to provide the prompt template like this: `"Source Text: {input_text}, Custom instruction : {instruction}"`.
-
-- `OPENAI_MODEL` (default: `gpt-3.5-turbo`) : The OpenAI model to use.
-
-- `OPENAI_PROVIDER` (available options: `openai`, `azure`, `ollama`, default - `openai`) : The OpenAI provider to use.
-
-- `TEMPERATURE` (default: `0.7`): The temperature to use for the model.
-
-- `NUM_RESPONSES` (default: `1`): The number of responses to generate in `<TextArea>` output fields. Useful if you want
-  to generate multiple responses and let the user rank the best one.
-
-- `OPENAI_API_KEY`: The OpenAI or Azure API key to use. Must be set before deploying the server.
-
-### Azure Configuration
-
-If you are using Azure as your OpenAI provider (`OPENAI_PROVIDER=azure`), you need to specify the following environment
-variables:
-
-- `AZURE_RESOURCE_ENDPOINT`: This is the endpoint for your Azure resource. It should be set to the appropriate value
-  based on your Azure setup.
-
-- `AZURE_DEPLOYMENT_NAME`: This is the name of your Azure deployment. It should match the name you've given to your
-  deployment in Azure.
-
-- `AZURE_API_VERSION`: This is the version of the Azure API you are using. The default value is `2023-05-15`.
-
-### Ollama Configuration
-
-If you are using Ollama as your LLM provider (`OPENAI_PROVIDER=ollama`), you need to specify the following environment variables:
-
-- `OPENAI_MODEL` : The Ollama model to use, for example `llama3`.
-
-- `OLLAMA_ENDPOINT`: This is the endpoint for your Ollama endpoint. It should be set to the appropriate value based on your setup. If you are running it locally, then it can typically be reached on `http://host.docker.internal:11434/v1/`
--- label_studio_ml/examples/rag_quickstart/model.py
+++ label_studio_ml/examples/rag_quickstart/model.py
@@ -1,8 +1,6 @@
 import logging
-import difflib
 import os
 
-from io import BytesIO
 from typing import Union, List, Dict, Optional, Any, Tuple
 
 from label_studio_ml.model import LabelStudioMLBase
@@ -10,6 +8,9 @@ from label_studio_ml.response import ModelResponse
 from label_studio_sdk.label_interface.objects import PredictionValue
 from label_studio_sdk.label_interface.control_tags import ControlTag, ObjectTag
 
+from datasets import Dataset
+from ragas.metrics import faithfulness, answer_relevancy
+from ragas import evaluate
 from langchain_chroma import Chroma
 from langchain_core.documents import Document
 from langchain_community.document_loaders.directory import DirectoryLoader
@@ -27,21 +28,27 @@ import nltk
 rag_chain = None
 last_prompts = None
 
+
 def iife_rag():
     global rag_chain
     if rag_chain:
         return rag_chain
 
-    nltk.download('punkt')
-    nltk.download('averaged_perceptron_tagger')
+    nltk.download("punkt")
+    nltk.download("averaged_perceptron_tagger")
     llm = ChatOpenAI(model=RagQuickstart.OPENAI_RAG_MODEL)
 
-    loader = DirectoryLoader(RagQuickstart.DOCUMENTATION_PATH, glob=RagQuickstart.DOCUMENTATION_GLOB)
+    loader = DirectoryLoader(
+        RagQuickstart.DOCUMENTATION_PATH, glob=RagQuickstart.DOCUMENTATION_GLOB
+    )
     docs = loader.load()
 
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
     splits = text_splitter.split_documents(docs)
-    vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings(model=RagQuickstart.OPENAI_EMBEDDING_MODEL))
+    vectorstore = Chroma.from_documents(
+        documents=splits,
+        embedding=OpenAIEmbeddings(model=RagQuickstart.OPENAI_EMBEDDING_MODEL),
+    )
     retriever = vectorstore.as_retriever()
 
     system_prompt = (
@@ -68,99 +75,108 @@ def iife_rag():
 def format_documentation(documentation: List[Document]) -> List[str]:
     """Format documentation for display in a TextArea"""
 
-    return [f"source: {d.metadata.get('source', 'source unknown')}\n\n{d.page_content}" for d in documentation]
+    return [
+        f"source: {d.metadata.get('source', 'source unknown')}\n\n{d.page_content}"
+        for d in documentation
+    ]
+
 
 def get_rag_response(messages: Union[List[Dict], str], params, *args, **kwargs):
-    """
-    """
+    """ """
     logger.debug(f"rag request: {messages}, params={params}")
     rag = iife_rag()
     response = rag.invoke({"input": messages})
     print(f'rag response: {response=} {response["answer"]=} {response["context"]=}')
     return response
 
-def get_evaluation(response) -> Tuple[str, float]:
+
+def get_ragas_evaluation(response) -> Tuple[List[str], float]:
+    ragas_dataset = {
+        "question": [response["input"]],
+        "answer": [response["answer"]],
+        "contexts": [format_documentation(response["context"])],
+    }
+    dataset = Dataset.from_dict(ragas_dataset)
+    score = evaluate(dataset, metrics=[faithfulness, answer_relevancy])
+
+    results = []
+    for item in score.items():
+        rounded = "{:.2f}".format(round(item[1], 2))
+        results.append(f"{item[0]}: {rounded}")
+
+    return results, (score["faithfulness"] + score["answer_relevancy"]) / 2
+
+
+def get_textual_evaluation(response) -> Tuple[str, float]:
     llm = ChatOpenAI(model=RagQuickstart.OPENAI_EVALUATION_MODEL)
     messages = [
-        ("system", "Evaluate the following answer to the given question (along with any additional instructions provided), both for correctness as well as whether it is supported by the given documentation."),
+        (
+            "system",
+            "Evaluate the following answer to the given question (along with any additional instructions provided), "
+            "both for correctness as well as whether it is supported by the given documentation. All claims about "
+            "documentation must cite specific documentation that was provided, along with the path to the source. Do "
+            "not use first person or provide a revised answer.",
+        ),
         ("human", "QUESTION AND ADDITIONAL INSTRUCTIONS: " + response["input"]),
         ("human", "ANSWER: " + response["answer"]),
         ("human", "DOCUMENTATION: " + str(response["context"])),
     ]
     evaluation = llm.invoke(messages)
-    print(f'\n\n{evaluation=}\n\n')
+    print(f"\n\n{evaluation=}\n\n")
 
-    float_eval = None
-    attempt = 0
-    while float_eval is None and attempt <= 3:
-        attempt += 1
-        print(f'attempt {attempt} to get numerical evaluation')
-        numerical_eval_messages = [
-            ("system", "Respond with ONLY a float between 0 and 1 inclusive, that represents the quality of the answer according to the given evaluation. 0 means the answer is completely wrong and unsupported by documentation, 1 means the answer is completely correct and supported by documentation."),
-            ("human", "EVALUATION: " + evaluation.content),
-        ]
-        numerical_evaluation = llm.invoke(numerical_eval_messages)
-        print(f'\n\n{numerical_evaluation=}\n\n')
+    return evaluation.content
 
-        try:
-            float_eval = float(numerical_evaluation.content)
-        except ValueError:
-            print('Could not convert numerical evaluation to float', numerical_evaluation.content)
-
-        if float_eval < 0:
-            float_eval = 0.0
-        elif float_eval > 1:
-            float_eval = 1.0
-
-    return evaluation.content, float_eval
 
 class RagQuickstart(LabelStudioMLBase):
-    """
-    """
+    """ """
+
     DOCUMENTATION_PATH = os.getenv("DOCUMENTATION_PATH")
     DOCUMENTATION_GLOB = os.getenv("DOCUMENTATION_GLOB")
-    OPENAI_KEY = os.getenv('OPENAI_API_KEY')
-    OPENAI_EMBEDDING_MODEL = os.getenv('OPENAI_EMBEDDING_MODEL', 'text-embedding-3-small')
-    OPENAI_RAG_MODEL = os.getenv('OPENAI_RAG_MODEL', 'gpt-3.5-turbo-0125')
-    OPENAI_EVALUATION_MODEL = os.getenv('OPENAI_EVALUATION_MODEL', 'gpt-3.5-turbo-0125')
+    OPENAI_KEY = os.getenv("OPENAI_API_KEY")
+    OPENAI_EMBEDDING_MODEL = os.getenv(
+        "OPENAI_EMBEDDING_MODEL", "text-embedding-3-small"
+    )
+    OPENAI_RAG_MODEL = os.getenv("OPENAI_RAG_MODEL", "gpt-3.5-turbo-0125")
+    OPENAI_EVALUATION_MODEL = os.getenv("OPENAI_EVALUATION_MODEL", "gpt-3.5-turbo-0125")
     # if set, additional instructions will be cleared between tasks
-    CLEAR_ADDITIONAL_INSTRUCTIONS_BETWEEN_TASKS = bool(int(os.getenv("CLEAR_ADDITIONAL_INSTRUCTIONS_BETWEEN_TASKS", 1)))
-    PROMPT_TEMPLATE = os.getenv("PROMPT_TEMPLATE", '**Question to answer**:\n\n"{text}"\n\n**Additional instructions (may be empty)**:\n\n"{prompt}"')
+    CLEAR_ADDITIONAL_INSTRUCTIONS_BETWEEN_TASKS = bool(
+        int(os.getenv("CLEAR_ADDITIONAL_INSTRUCTIONS_BETWEEN_TASKS", 1))
+    )
+    PROMPT_TEMPLATE = os.getenv(
+        "PROMPT_TEMPLATE",
+        '**Question to answer**:\n\n"{text}"\n\n**Additional instructions (may be empty)**:\n\n"{prompt}"',
+    )
     SUPPORTED_INPUTS = ("Text",)
 
     def setup(self):
         pass
 
     def _get_text(self, task_data, object_tag):
-        """
-        """
+        """ """
         data = task_data.get(object_tag.value_name)
         return data
 
     def _get_prompts(self, context, prompt_tag) -> List[str]:
-        """Getting prompt values
-        """
+        """Getting prompt values"""
         if context:
             # Interactive mode - get prompt from context
-            result = context.get('result')
+            result = context.get("result")
             for item in result:
-                if item.get('from_name') == prompt_tag.name:
-                    return item['value']['text']
+                if item.get("from_name") == prompt_tag.name:
+                    return item["value"]["text"]
         # Initializing - get existing prompt from storage
         elif prompt := self.get(prompt_tag.name):
             return [prompt]
 
         return []
 
-
     def _find_textarea_tag(self, prompt_tag, object_tag):
-        """Free-form text predictor
-        """
+        """Free-form text predictor"""
         li = self.label_interface
 
         try:
             textarea_from_name, _, _ = li.get_first_tag_occurence(
-                'TextArea',
+                "TextArea",
                 self.SUPPORTED_INPUTS,
                 name_filter=lambda s: s != prompt_tag.name,
                 to_name_filter=lambda s: s == object_tag.name,
@@ -171,30 +187,40 @@ class RagQuickstart(LabelStudioMLBase):
             return None
 
     def _find_tags_with_prefix(self, prefix, tag_type) -> Tuple[ControlTag, ObjectTag]:
-        """Find tags in the config matching prefix and tag type
-        """
+        """Find tags in the config matching prefix and tag type"""
         li = self.label_interface
         tag_from_name, tag_to_name, value = li.get_first_tag_occurence(
             tag_type,
             # supported input types
             self.SUPPORTED_INPUTS,
             # if multiple <TextArea> are presented, use one with prefix specified
-            name_filter=lambda s: s.startswith(prefix))
+            name_filter=lambda s: s.startswith(prefix),
+        )
 
         return li.get_control(tag_from_name), li.get_object(tag_to_name)
 
     def _validate_tags(self, textarea_tag: str) -> None:
         if not textarea_tag:
-            raise ValueError('No supported tags found: <TextArea>')
+            raise ValueError("No supported tags found: <TextArea>")
 
-    def _generate_normalized_prompt(self, text: str, prompt: str, task_data: Dict) -> str:
-        """
-        """
+    def _generate_normalized_prompt(
+        self, text: str, prompt: str, task_data: Dict
+    ) -> str:
+        """ """
         return self.PROMPT_TEMPLATE.format(text=text, prompt=prompt)
 
-    def _generate_response_regions(self, answer: str, float_eval: Optional[float], str_eval: str, documentation: List[str], prompt_tag, textarea_tag: ControlTag, prompts: List[str]) -> List:
-        """
-        """
+    def _generate_response_regions(
+        self,
+        answer: str,
+        ragas_eval: List[str],
+        float_eval: Optional[float],
+        str_eval: str,
+        documentation: List[str],
+        prompt_tag,
+        textarea_tag: ControlTag,
+        prompts: List[str],
+    ) -> List:
+        """ """
         regions = []
 
         if textarea_tag:
@@ -203,46 +229,69 @@ class RagQuickstart(LabelStudioMLBase):
         # not sure why we need this but it was in the original code
         regions.append(prompt_tag.label(text=prompts))
 
-        doc_tag, _ = self._find_tags_with_prefix('documentation', 'TextArea')
-        eval_tag, _ = self._find_tags_with_prefix('evaluation', 'TextArea')
-        eval_number_tag, _ = self._find_tags_with_prefix('float_eval', 'Number')
+        doc_tag, _ = self._find_tags_with_prefix("documentation", "TextArea")
+        eval_tag, _ = self._find_tags_with_prefix("evaluation", "TextArea")
+        ragas_tag, _ = self._find_tags_with_prefix("ragas", "TextArea")
+        eval_number_tag, _ = self._find_tags_with_prefix("float_eval", "Number")
 
         regions.append(doc_tag.label(text=documentation))
         regions.append(eval_tag.label(text=[str_eval]))
+        regions.append(ragas_tag.label(text=ragas_eval))
         regions.append(eval_number_tag.label(number=int(float_eval * 100)))
 
         return regions
 
-    def _predict_single_task(self, task_data: Dict, prompt_tag: Any, object_tag: Any, prompt: str,
-                             textarea_tag: ControlTag, prompts: List[str]) -> Dict:
-        """
-        """
+    def _predict_single_task(
+        self,
+        task_data: Dict,
+        prompt_tag: Any,
+        object_tag: Any,
+        prompt: str,
+        textarea_tag: ControlTag,
+        prompts: List[str],
+    ) -> Dict:
+        """ """
         text = self._get_text(task_data, object_tag)
         norm_prompt = self._generate_normalized_prompt(text, prompt, task_data)
         response = get_rag_response(norm_prompt, self.extra_params)
-        evaluation, float_eval = get_evaluation(response)
+        textual_evaluation = get_textual_evaluation(response)
+        ragas_evaluation, ragas_evaluation_float = get_ragas_evaluation(response)
 
-        regions = self._generate_response_regions(response["answer"], float_eval, evaluation, format_documentation(response["context"]), prompt_tag, textarea_tag, prompts)
+        regions = self._generate_response_regions(
+            response["answer"],
+            ragas_evaluation,
+            ragas_evaluation_float,
+            textual_evaluation,
+            format_documentation(response["context"]),
+            prompt_tag,
+            textarea_tag,
+            prompts,
+        )
 
-        return PredictionValue(result=regions, score=(float_eval or 0.1), model_version=str(self.model_version))
+        return PredictionValue(
+            result=regions,
+            score=ragas_evaluation_float,
+            model_version=str(self.model_version),
+        )
 
-    def predict(self, tasks: List[Dict], context: Optional[Dict] = None, **kwargs) -> ModelResponse:
-        """
-        """
+    def predict(
+        self, tasks: List[Dict], context: Optional[Dict] = None, **kwargs
+    ) -> ModelResponse:
+        """ """
         global last_prompts
         predictions = []
 
         # prompt tag contains the prompt in the config
         # object tag contains what we plan to label
-        prompt_tag, object_tag = self._find_tags_with_prefix('prompt', 'TextArea')
+        prompt_tag, object_tag = self._find_tags_with_prefix("prompt", "TextArea")
         prompts = self._get_prompts(context, prompt_tag)
 
         if self.CLEAR_ADDITIONAL_INSTRUCTIONS_BETWEEN_TASKS:
             # hack: let's say if there are no predictions, then there are no prompts
-            if not tasks[0].get('predictions'):
+            if not tasks[0].get("predictions"):
                 prompts = []
         else:
-            if not tasks[0].get('predictions') and last_prompts:
+            if not tasks[0].get("predictions") and last_prompts:
                 prompts = last_prompts
         last_prompts = prompts
 
@@ -253,9 +302,10 @@ class RagQuickstart(LabelStudioMLBase):
 
         for task in tasks:
             # preload all task data fields, they are needed for prompt
-            task_data = self.preload_task_data(task, task['data'])
-            pred = self._predict_single_task(task_data, prompt_tag, object_tag, prompt,
-                                                textarea_tag, prompts)
+            task_data = self.preload_task_data(task, task["data"])
+            pred = self._predict_single_task(
+                task_data, prompt_tag, object_tag, prompt, textarea_tag, prompts
+            )
             predictions.append(pred)
 
         return ModelResponse(predictions=predictions)
