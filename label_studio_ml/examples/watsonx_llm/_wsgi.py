97aefa82c43fc422be5b58dcae211e5bb002df94
Micaela Kaplan <kaplan.micaela@gmail.com>
2024-08-07 14:32:59 -0400

    change wsgi app

--- label_studio_ml/examples/watsonx_llm/_wsgi.py
+++ label_studio_ml/examples/watsonx_llm/_wsgi.py
@@ -1,8 +1,11 @@
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from flask import Flask
 
 from data_wsgi import application as data
 import model_wsgi as model
 
-app = DispatcherMiddleware(model.application, {
-    '/data': data
+app = Flask(__name__)
+
+app.wsgi_app = DispatcherMiddleware(model.application.wsgi_app, {
+    '/data': data.wsgi_app
 })
