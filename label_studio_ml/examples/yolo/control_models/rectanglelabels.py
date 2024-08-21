206886667a88e2edd3a362e49fade4a58e98af01
makseq-ubnt <makseq@gmail.com>
2024-08-21 13:57:01 +0300

    Revert rect labels model back

--- label_studio_ml/examples/yolo/control_models/rectanglelabels.py
+++ label_studio_ml/examples/yolo/control_models/rectanglelabels.py
@@ -14,7 +14,7 @@ class RectangleLabelsModel(ControlModel):
     Class representing a RectangleLabels (bounding boxes) control tag for YOLO model.
     """
     type = 'RectangleLabels'
-    model_path = 'yolov8n-obb.pt'
+    model_path = 'yolov8m.pt'
 
     @classmethod
     def is_control_matched(cls, control) -> bool:
