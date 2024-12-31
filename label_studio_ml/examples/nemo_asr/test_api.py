0fb0d66c1af6de94fcfd698b670593018603319b
makseq <makseq@gmail.com>
2024-12-31 14:09:11 +0000

    fix: Pytests spacy

--- label_studio_ml/examples/nemo_asr/test_api.py
+++ label_studio_ml/examples/nemo_asr/test_api.py
@@ -27,7 +27,49 @@ def client():
 def test_predict(client):
     request = json.loads('{"tasks": [{"id": 102651352, "data": {"audio": "https://htx-pub.s3.amazonaws.com/datasets/audio/f2bjrop1.1.wav"}, "meta": {}, "created_at": "2024-04-08T15:52:07.473205Z", "updated_at": "2024-04-08T15:52:07.473210Z", "is_labeled": false, "overlap": 1, "inner_id": 2, "total_annotations": 0, "cancelled_annotations": 0, "total_predictions": 0, "comment_count": 0, "unresolved_comment_count": 0, "last_comment_updated_at": null, "project": 62075, "updated_by": null, "file_upload": 1430391, "comment_authors": [], "annotations": [], "predictions": []}], "project": "62075.1712591487", "label_config": "<View>\\n  <Audio name=\\"audio\\" value=\\"$audio\\" zoom=\\"true\\" hotkey=\\"ctrl+enter\\" />\\n  <Header value=\\"Provide Transcription\\" />\\n  <TextArea name=\\"transcription\\" toName=\\"audio\\"\\n            rows=\\"4\\" editable=\\"true\\" maxSubmissions=\\"1\\" />\\n</View>", "params": {"login": null, "password": null, "context": null}}')
 
-    expected_response = json.loads('{"results":[{"model_version":"NemoASR-v0.0.1","result":[{"from_name":"transcription","to_name":"audio","type":"textarea","value":{"text":["ffected to be named in march it may be the most important appointment governor michael dukakis makes during the remainder of his administration and one of the toughest s w b ar as murgr menikovi"]}}],"score":1.0}]}\n')
+    expected_response = {
+        "results": [
+            {
+            "model_version": "SpacyMLBackend-v0.0.1",
+            "result": [
+                {
+                "from_name": "ner_tags",
+                "to_name": "text",
+                "type": "labels",
+                "value": {
+                    "end": 10,
+                    "labels": ["PERSON"],
+                    "start": 0,
+                    "text": "Katy Perry"
+                }
+                },
+                {
+                "from_name": "ner_tags",
+                "to_name": "text",
+                "type": "labels",
+                "value": {
+                    "end": 23,
+                    "labels": ["ORG"],
+                    "start": 15,
+                    "text": "Pharrell"
+                }
+                },
+                {
+                "from_name": "ner_tags",
+                "to_name": "text",
+                "type": "labels",
+                "value": {
+                    "end": 83,
+                    "labels": ["PRODUCT"],
+                    "start": 47,
+                    "text": "Apple Watches http://t.co/k6k3SdwShP"
+                }
+                }
+            ],
+            "score": 0.0
+            }
+        ]
+    }
 
     response = client.post('/predict', data=json.dumps(request), content_type='application/json')
     assert response.status_code == 200
