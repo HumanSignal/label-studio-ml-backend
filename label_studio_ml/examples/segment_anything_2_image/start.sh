cee6e9d3828b510774109eafdcd2f125692c32db
Micaela Kaplan <kaplan.micaela@gmail.com>
2025-02-10 10:25:43 -0500

    update wsgi

--- label_studio_ml/examples/segment_anything_2_image/start.sh
+++ label_studio_ml/examples/segment_anything_2_image/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Execute the gunicorn command
-exec gunicorn --bind :${PORT:-9090} --workers ${WORKERS:-1} --threads ${THREADS:-4} --timeout 0 app._wsgi:app
+exec gunicorn --bind :${PORT:-9090} --workers ${WORKERS:-1} --threads ${THREADS:-4} --timeout 0 ~.app._wsgi:app
