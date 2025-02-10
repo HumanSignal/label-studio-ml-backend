3d43b1051195829d57ee3555a6f786cccac05d3d
Micaela Kaplan <kaplan.micaela@gmail.com>
2025-02-10 10:28:42 -0500

    update wsgi

--- label_studio_ml/examples/segment_anything_2_image/start.sh
+++ label_studio_ml/examples/segment_anything_2_image/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Execute the gunicorn command
-exec gunicorn --bind :${PORT:-9090} --workers ${WORKERS:-1} --threads ${THREADS:-4} --timeout 0 root.app._wsgi:app
+exec gunicorn --bind :${PORT:-9090} --workers ${WORKERS:-1} --threads ${THREADS:-4} --timeout 0 pytorch/pytorch:2.5.1-cuda12.4-cudnn9-runtime.app._wsgi:app
