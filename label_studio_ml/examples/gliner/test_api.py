505796380cabe7d845dc0f57cee3cd24ba5be561
Micaela Kaplan <kaplan.micaela@gmail.com>
2025-02-21 12:31:56 -0500

    update tests

--- label_studio_ml/examples/gliner/test_api.py
+++ label_studio_ml/examples/gliner/test_api.py
@@ -58,7 +58,7 @@ def test_predict(client):
     expected_response = {"results": [{"model_version": "GLiNERModel-v0.0.1", "result": [
         {"from_name": "label", "score": 0.9220, "to_name": "text", "type": "labels",
          "value": {"end": 11, "labels": ["Medication/Vaccine"], "start": 0, "text": "atomoxetine"}},
-        {"from_name": "label", "score": 0.7053, "to_name": "text", "type": "labels",
+        {"from_name": "label", "score": 0.8706, "to_name": "text", "type": "labels",
          "value": {"end": 65, "labels": ["Medication/Vaccine"], "start": 32,
                    "text": "norepinephrine reuptake inhibitor"}}], "score": 0.7053}]}
 
